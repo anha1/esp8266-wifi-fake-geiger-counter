55c184b5fe0286a88751d9c54dd17c05c4311240
Anton Haidai <anton.haidai@gmail.com>
2019-04-03 20:47:21 +0300

    improvements

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # esp8266-wifi-fake-geiger-counter
-esp8266-based fake Geiger counter, reacts on WiFi networks signal level.
+esp8266-based fake Geiger counter, reacts on WiFi networks signal level. By default, reacts on the strongest signal, but it is possible to fix a single network by defining IS_SINGLE_NETWORK/SINGLE_NETWORK_SSID constants.
 
+![Sketch](https://raw.githubusercontent.com/anha1/esp8266-wifi-fake-geiger-counter/master/img/sketch_bb.png) 
 
 [https://www.youtube.com/watch?v=JckX73Gao08](https://www.youtube.com/watch?v=JckX73Gao08)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include "ESP8266WiFi.h"
-#define SOUND_PIN 5
+#define SOUND_PIN 5 // this is the D1 pin on NodeMCU boards
+#define LED_PIN 2   // embedded LED
+
+#define IS_SINGLE_NETWORK false
+#define SINGLE_NETWORK_SSID String("MyWifiNet")
+
+#define IS_PRINT_NETWORKS false
 
 volatile float maxDelay = 600000;
 volatile float currentDelay = 600000;
@@ -12,22 +18,32 @@ void generateCurrentDelay() {
     currentDelay = random(1, maxDelay);
 }
 
+float signalToMaxDelay(int signal) {
+    return exp((signal - 15.) / 10.) * 5.;
+}
+
 void onCompleteScan(int networksFound) {
     float strongestSignal = 10000;
 
-    for(int i =0; i < networksFound; i++) {
+    for (int i =0; i < networksFound; i++) {
+        if (IS_SINGLE_NETWORK && !(SINGLE_NETWORK_SSID.equals(WiFi.SSID(i)))) {
+            continue;
+        }
+
         int currentSignal = abs(WiFi.RSSI(i));
         if (currentSignal < strongestSignal) {
             strongestSignal = currentSignal;
         }
-        /*
-        Serial.print("Network name: ");
-        Serial.println(WiFi.SSID(i));
-        Serial.print("Signal strength: ");
-        Serial.println(WiFi.RSSI(i));
-        */
+        
+        if (IS_PRINT_NETWORKS) {
+            Serial.print("Network name: ");
+            Serial.println(WiFi.SSID(i));
+            Serial.print("Signal strength: ");
+            Serial.println(WiFi.RSSI(i));
+        }
+        
     }
-    maxDelay =  exp((strongestSignal - 15.) / 10.) * 5.;
+    maxDelay = signalToMaxDelay(strongestSignal);
     generateCurrentDelay();
     isNeedScan = true;
 }
@@ -40,7 +56,9 @@ void scanAsync() {
 void setup() {
     Serial.begin(9600);
     pinMode(SOUND_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
     digitalWrite(SOUND_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
 
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
@@ -52,8 +70,11 @@ void setup() {
 
 void beep() {
     digitalWrite(SOUND_PIN, HIGH);
+    digitalWrite(LED_PIN, LOW);
     delay(1);
     digitalWrite(SOUND_PIN, LOW);
+    delay(4);
+    digitalWrite(LED_PIN, HIGH);
 }
 
 void loop() {
